9c4ebd7a080a344b0bcee5241cb40ab077df36ee
johnrees <john@bitsushi.com>
2013-06-05 16:24:20 +0200

    isotope work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
   def vimeo_player src
-    content_tag :iframe, nil, allowfullscreen: true, frameborder: 0, mozallowfullscreen: true, src: "http://player.vimeo.com/video/#{src}", webkitallowfullscreen: "", width: 960, height: 540
+    content_tag :iframe,
+      nil,
+      allowfullscreen: true,
+      frameborder: 0,
+      mozallowfullscreen: true,
+      src: "http://player.vimeo.com/video/#{src}?color=F9A518",
+      webkitallowfullscreen: true,
+      width: 960,
+      height: 540
   end
 
   def link_to_with_active_state text, link, html_options = {}
