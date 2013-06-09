5ae5d7a1963506f46ef1d495555a2e30482be634
johnrees <john@bitsushi.com>
2013-06-09 12:28:35 +0200

    loader

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def vimeo_player src
-    # content_tag :div, id: 'vimeo-container' do
+    content_tag :div, id: 'vimeo-container' do
     content_tag :iframe,
       nil,
       allowfullscreen: true,
@@ -12,7 +12,7 @@ module ApplicationHelper
       width: 990,
       height: 558
   end
-  # end
+  end
 
   def link_to_with_active_state text, link, html_options = {}
     link_to text, link, class: ('active' if request.path == link), data: { name: text.parameterize }
