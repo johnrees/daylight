2330901eed0d649875c07e525dab338a68966884
John Rees <john@bitsushi.com>
2013-10-02 19:03:34 +0200

    map

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Daylight::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( admin.js admin.css modernizr.js )
+  config.assets.precompile += %w( admin.js admin.css modernizr.js map.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
